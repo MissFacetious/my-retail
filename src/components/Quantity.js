76c66c7aaba7d5caec5df59c98161f1ae2447e54
Lisa Walkosz-Migliacio <evilisa@gmail.com>
2019-02-05 11:33:31 -0600

    Correctly working quantity widget

--- src/components/Quantity.js
+++ src/components/Quantity.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 
 class Quantity extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: this.props.quantity
+    };
+    // bind the functions as they do not do so automatically
+    this.quantityChange = this.quantityChange.bind(this);
+  }
+
+  // this function will be called when clicking the minus or plus to change the quantity
+  // and display it on the page
   quantityChange(event) {
     var changeQuantity = event.target.dataset.id;
-    if (this.props.quantity > 1) { // we shouldn't have quantity be less than 1 to add
-      this.props.quantity += changeQuantity;
+    if (changeQuantity != null) {
+      changeQuantity = parseInt(changeQuantity, 10);
+    }
+    if (this.state.quantity > 1 || changeQuantity > 0) { // we shouldn't have quantity be less than 1 to add
+      this.state.quantity += changeQuantity;
+      this.setState({ quantity: this.state.quantity });
     }
   }
 
@@ -16,7 +31,7 @@ class Quantity extends Component {
           &nbsp;
           <span className="right">
             <span className="minus" data-id="-1" onClick={this.quantityChange}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-              {this.props.quantity}
+              {this.state.quantity}
             <span className="plus" data-id="1" onClick={this.quantityChange}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
           </span>
         </div>
