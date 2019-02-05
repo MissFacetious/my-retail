81150cd001d0e453d8cde4842a60aa5fbc126dd6
Lisa Walkosz-Migliacio <evilisa@gmail.com>
2019-02-05 11:20:38 -0600

    implement quantity box

--- src/components/Quantity.js
+++ src/components/Quantity.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 
 class Quantity extends Component {
 
-
+  quantityChange(event) {
+    var changeQuantity = event.target.dataset.id;
+    if (this.props.quantity > 1) { // we shouldn't have quantity be less than 1 to add
+      this.props.quantity += changeQuantity;
+    }
+  }
 
   render() {
     return (
-        <p>Quantity</p>
+        <div className="boxed">
+          <span className="quantity left">quantity: </span>
+          &nbsp;
+          <span className="right">
+            <span className="minus" data-id="-1" onClick={this.quantityChange}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+              {this.props.quantity}
+            <span className="plus" data-id="1" onClick={this.quantityChange}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+          </span>
+        </div>
       );
   };
 }
