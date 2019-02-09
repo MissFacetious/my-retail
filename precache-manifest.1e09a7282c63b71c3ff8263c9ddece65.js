faa6c1ee26ff0eda865886114ef3b6d200d67418
Lisa Walkosz-Migliacio <evilisa@gmail.com>
2019-02-08 21:37:45 -0600

    Updates

---
+++ precache-manifest.1e09a7282c63b71c3ff8263c9ddece65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3376434e2087b8723b6",
+    "url": "/my-retail/static/js/runtime~main.b3376434.js"
+  },
+  {
+    "revision": "4ad337243d7d67d8ff75",
+    "url": "/my-retail/static/js/main.4ad33724.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/my-retail/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "4ad337243d7d67d8ff75",
+    "url": "/my-retail/static/css/main.0aa0db2c.chunk.css"
+  },
+  {
+    "revision": "5b330adb04c6357a598a56797ac6b2d9",
+    "url": "/my-retail/index.html"
+  }
+];
